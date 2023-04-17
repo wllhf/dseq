f745a9ce985c7b8b56590e44fad66307d6fc8f6a
Manuel Woellhaf <woellhaf@tu-berlin.de>
2023-04-17 21:55:09 +0200

    Added some beauty to the lin gaussian ssm.

--- src/ssm.py
+++ src/ssm.py
@@ -1,36 +1,61 @@
 import numpy as np
 
 
-def lin_gaussian_ssm(
-        dim_state=1, dim_obs=1, seq_len=10, batch_size=8,
-        A=np.array([[0.5]]), C=np.array([[1]]), Q=np.array([[1]]), R=np.array([[1]])
-        ):
+def lin_gaussian_ssm(A, C, Q, R, seq_len, num_samples, seed=0, dtype=np.float32):
+    """
+    Linear Gaussian State Space Model.
+      state:
+        x_t+1 = A*x_t + N(0, Q)
+      observation
+        y_t = C*x_t + N(0, R)
 
-    sta = np.empty([batch_size, seq_len, dim_state])
-    obs = np.empty([batch_size, seq_len, dim_obs])
+    args:
+      A: list of lists with d(x) x d(x)
+      C: list of lists with d(y) x d(x)
+      Q: list of lists with d(x) x d(x)
+      R: list of lists with d(y) x d(y)
+    return:
+      sta, cov, obs
+    """
+    random_state = np.random.RandomState(seed)
+
+    dim_state = len(A)
+    dim_obs = len(C)
+
+    A = np.array(A, dtype=dtype)
+    C = np.array(C, dtype=dtype)
+    Q = np.array(Q, dtype=dtype)
+    R = np.array(R, dtype=dtype)
+
+    cov = np.zeros([num_samples, seq_len, dim_state, dim_state], dtype=dtype)
+    sta = np.zeros([num_samples, seq_len, dim_state], dtype=dtype)
+    obs = np.zeros([num_samples, seq_len, dim_obs], dtype=dtype)
 
     I = np.eye(dim_state)
     loc_sta = np.zeros(dim_state)
-    loc_obs = np.zeros(dim_state)
+    loc_obs = np.zeros(dim_obs)
 
     def prod(M, v):
         return np.einsum('ij,ki->ki', M, v)
 
-    sta[:, 0, :] = np.random.normal(loc=loc_sta, scale=I, size=(batch_size, dim_state))
-    obs[:, 0, :] = prod(C, sta[:, 0, :]) + np.random.normal(loc=loc_sta, scale=R, size=(batch_size, dim_obs))
+    sta[:, 0, :] = loc_sta
+    obs[:, 0, :] = prod(C, sta[:, 0, :]) + random_state.normal(loc_obs, scale=R, size=(num_samples, dim_obs))
 
     for t in range(1, seq_len):
-        sta[:, t, :] = prod(A, sta[:, t-1, :]) + np.random.normal(loc=loc_obs, scale=Q, size=(batch_size, dim_state))
-        obs[:, t, :] = prod(C, sta[:, t, :]) + np.random.normal(loc=loc_sta, scale=R, size=(batch_size, dim_obs))
+        sta[:, t, :] = prod(A, sta[:, t-1, :]) + random_state.normal(loc=loc_sta, scale=Q, size=(num_samples, dim_state))
+        obs[:, t, :] = prod(C, sta[:, t, :]) + random_state.normal(loc=loc_obs, scale=R, size=(num_samples, dim_obs))
 
-    return sta, obs
+    # constant covariance
+    cov = np.broadcast_to(Q, (num_samples, seq_len, dim_state, dim_state))
+    return sta, cov, obs
 
 
 
 if __name__ == '__main__':
+    import math
     import matplotlib.pyplot as plt
-    seq_len = 10
-    sta, obs = lin_gaussian_ssm(seq_len=seq_len, batch_size=2)
+    seq_len = 5
+    sta, cov, obs = lin_gaussian_ssm(seq_len=seq_len, num_samples=1, A=[[0.5]], C=[[1.0]], Q=[[0.5]], R=[[0.1]])
 
     t = range(seq_len)
     plt.plot(t, sta[0, ...])
