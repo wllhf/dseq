b4d351ba32dee753d2cdc41fa3ff95ff42d666ba
Manuel Woellhaf <woellhaf@tu-berlin.de>
2023-04-20 09:45:10 +0200

    Add log_likelihood function for lin gaussian ssm to util.

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import tensorflow_probability as tfp
 
 from data.ssm import lin_gaussian_ssm
 
+tfpd = tfp.distributions
 
 
 ssm_params = {
@@ -15,8 +17,8 @@ ssm_params = {
     'SEQ_LEN': 20,
     'NUM_SAMPLES': {'trn': 20480, 'val': 128, 'tst': 100},
     'BATCH_SIZE': 1024,
-    'LEARNING_RATE': 0.001,
-    'NUM_EPOCHS': 200,
+    'LEARNING_RATE': 0.0005,
+    'NUM_EPOCHS': 10,
     'SEEDS': {'trn': 0, 'val': 1234, 'tst': 9000},
     'MODEL_PATH': '~/proj/dpf_final/'
 }
@@ -27,6 +29,11 @@ def ssm_loader(params, mode='trn'):
         num_samples=params['NUM_SAMPLES'][mode], seq_len=params['SEQ_LEN'],
         A=params['SSM_A'], C=params['SSM_C'], Q=params['SSM_Q'], R=params['SSM_R'],
         seed=params['SEEDS'][mode])
+
+
+def ssm_log_likelihood(params, target_state, target_obs):
+    obs_loc = np.einsum('ij,ki->ki', np.array(params['SSM_C']), target_state[..., 0])[..., None]
+    return tfpd.MultivariateNormalTriL(obs_loc, np.array(params['SSM_R'])).log_prob(target_obs)
 
 
 def ssm_plot(params, target_obs, target_state, target_cov, est_state, est_cov):
