2521470e6837ac5ea10287e7cf3c05ea3be45bf3
Manuel Woellhaf <woellhaf@tu-berlin.de>
2023-04-25 11:07:25 +0200

    Updated experiment utils.

--- src/utils.py
+++ src/utils.py
@@ -17,8 +17,8 @@ ssm_params = {
     'SEQ_LEN': 20,
     'NUM_SAMPLES': {'trn': 20480, 'val': 128, 'tst': 100},
     'BATCH_SIZE': 1024,
-    'LEARNING_RATE': 0.0005,
-    'NUM_EPOCHS': 10,
+    'LEARNING_RATE': 0.0001,
+    'NUM_EPOCHS': 100,
     'SEEDS': {'trn': 0, 'val': 1234, 'tst': 9000},
     'MODEL_PATH': '~/proj/dpf_final/'
 }
@@ -36,13 +36,26 @@ def ssm_log_likelihood(params, target_state, target_obs):
     return tfpd.MultivariateNormalTriL(obs_loc, np.array(params['SSM_R'])).log_prob(target_obs)
 
 
-def ssm_plot(params, target_obs, target_state, target_cov, est_state, est_cov):
+def ssm_plot(ax, params, target_obs, target_state, target_cov, est_state, est_cov):
     t = range(params['SEQ_LEN'])
-    plt.plot(t, target_state[0, ...], c='k')
-    plt.plot(t, target_state[0, ...]+np.sqrt(target_cov[0, ..., 0]), 'k--', )
-    plt.plot(t, target_state[0, ...]-np.sqrt(target_cov[0, ..., 0]), 'k--', )
-    plt.plot(t, est_state[0, ...], c='b')
-    plt.plot(t, est_state[0, ...]+np.sqrt(est_cov[0, ..., 0]), 'b--', )
-    plt.plot(t, est_state[0, ...]-np.sqrt(est_cov[0, ..., 0]), 'b--', )
-    plt.scatter(t, target_obs[0, ...], c='g')
-    plt.show()
+
+    ax.plot(t, target_state, c='k')
+    ax.plot(t, target_state + np.sqrt(target_cov), 'k--')
+    ax.plot(t, target_state - np.sqrt(target_cov), 'k--')
+
+    ax.plot(t, est_state, c='b')
+    ax.plot(t, est_state + np.sqrt(est_cov), 'b--')
+    ax.plot(t, est_state - np.sqrt(est_cov), 'b--')
+
+    ax.scatter(t, target_obs, c='g')
+
+def ssm_particle_plot(ax, params, target_obs, target_state, target_cov, particles, weights):
+    t = range(params['SEQ_LEN'])
+
+    ax.plot(t, target_state[0, ...], c='k')
+    ax.plot(t, target_state[0, ...]+np.sqrt(target_cov[0, ..., 0]), 'k--')
+    ax.plot(t, target_state[0, ...]-np.sqrt(target_cov[0, ..., 0]), 'k--')
+    ax.scatter(t, target_obs[0, ...], c='g')
+
+    for t in range(params['SEQ_LEN']):
+        ax.scatter([t]*particles.shape[1], particles[t, :], c=weights[t, :], marker='.', cmap='Blues')
