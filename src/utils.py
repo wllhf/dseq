a266950abb712e950d419e66802c37d17a5f2c65
Manuel Woellhaf <woellhaf@tu-berlin.de>
2023-04-26 13:12:50 +0200

    Collected updates to utility module.

--- src/utils.py
+++ src/utils.py
@@ -1,27 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import tensorflow as tf
 import tensorflow_probability as tfp
 
 from data.ssm import lin_gaussian_ssm
 
 tfpd = tfp.distributions
 
-
-ssm_params = {
-    'SSM_A': [[0.5]],
-    'SSM_C': [[1.0]],
-    'SSM_Q': [[0.5]],
-    'SSM_R': [[0.1]],
-    'DIM_OBS': 1,
-    'DIM_STATE': 1,
-    'SEQ_LEN': 20,
-    'NUM_SAMPLES': {'trn': 20480, 'val': 128, 'tst': 100},
-    'BATCH_SIZE': 1024,
-    'LEARNING_RATE': 0.0001,
-    'NUM_EPOCHS': 100,
-    'SEEDS': {'trn': 0, 'val': 1234, 'tst': 9000},
-    'MODEL_PATH': '~/proj/dpf_final/'
-}
+def softmax(x, axis=0):
+    ex = np.exp(x - np.max(x, axis=axis, keepdims=True))
+    return ex/ex.sum(axis=axis, keepdims=True)
 
 
 def ssm_loader(params, mode='trn'):
@@ -33,7 +21,8 @@ def ssm_loader(params, mode='trn'):
 
 def ssm_log_likelihood(params, target_state, target_obs):
     obs_loc = np.einsum('ij,ki->ki', np.array(params['SSM_C']), target_state[..., 0])[..., None]
-    return tfpd.MultivariateNormalTriL(obs_loc, np.array(params['SSM_R'])).log_prob(target_obs)
+    cov_chol = tf.linalg.cholesky(np.array(params['SSM_R']))
+    return tfpd.MultivariateNormalTriL(obs_loc, cov_chol).log_prob(target_obs)
 
 
 def ssm_plot(ax, params, target_obs, target_state, target_cov, est_state, est_cov):
@@ -52,10 +41,18 @@ def ssm_plot(ax, params, target_obs, target_state, target_cov, est_state, est_co
 def ssm_particle_plot(ax, params, target_obs, target_state, target_cov, particles, weights):
     t = range(params['SEQ_LEN'])
 
-    ax.plot(t, target_state[0, ...], c='k')
-    ax.plot(t, target_state[0, ...]+np.sqrt(target_cov[0, ..., 0]), 'k--')
-    ax.plot(t, target_state[0, ...]-np.sqrt(target_cov[0, ..., 0]), 'k--')
-    ax.scatter(t, target_obs[0, ...], c='g')
+    ax.plot(t, target_state, c='k')
+    ax.plot(t, target_state+np.sqrt(target_cov), 'k--')
+    ax.plot(t, target_state-np.sqrt(target_cov), 'k--')
 
-    for t in range(params['SEQ_LEN']):
-        ax.scatter([t]*particles.shape[1], particles[t, :], c=weights[t, :], marker='.', cmap='Blues')
+    weights = softmax(weights, axis=1)
+    for i in t:
+        ax.scatter([i]*particles.shape[1], particles[i, :], c=weights[i, :], marker='.', cmap='Blues')
+
+    # mean particle
+    ax.scatter(t, np.sum(particles*weights, axis=1), marker='x', c='r')
+    # max particle
+    best = np.take_along_axis(particles, np.expand_dims(np.argmax(weights, axis=1), axis=1), axis=1)
+    ax.scatter(t, best, marker='*', c='r')
+    # target states
+    ax.scatter(t, target_obs, c='g')
